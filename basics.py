76b31efa04a2578f95cb51e6d9332de93e787cc5
Raj Dubal <2000135236@hexaware.com>
2025-02-17 12:54:42 +0530

    Python oops

--- basics.py
+++ basics.py
@@ -79,3 +79,96 @@ def my_function(**kwargs):
         print(f'{key} , {value}')
 
 my_function(name="Alice",age=30, location="jersey")
+
+def print_country(country="india"):
+    print(country)
+
+print_country("sweden")
+
+def my_lambda(n):
+    return lambda a:a*n
+
+mydoubler=my_lambda(2)
+mytripler=my_lambda(3)
+
+print(mydoubler(11))
+print(mytripler(12))
+
+class Person:
+    def __init__(self,fname,lname):
+        self.fname=fname
+        self.lname=lname
+    def __str__(self):
+        return f'my first name is {self.fname} and my last name is {self.lname}'
+
+# p1=Person("John",23)
+# print(p1)
+# del p1.age
+# print(p1.age)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+
+print(x,x.graduationyear)
+
+mytuple=("apple","banana","cherry")
+myit=iter(mytuple)
+
+print(next(myit))
+
+def print_my_tuple(mytuple):
+    for item in mytuple:
+        print(item)
+    
+
+print_my_tuple(mytuple)
+
+class MyNumbers():
+    def __iter__(self):
+        self.a=0
+        return self
+    def __next__(self):
+        self.a+=1
+        return self.a
+        
+myobj=MyNumbers()
+myit=iter(myobj)
+print(next(myit))
+
+class Animal:
+    def make_sound(self):
+        raise NotImplementedError("subclass must implement abstract method")
+    def make_alt_sound(self):
+        raise NotImplementedError("subclass must implement abstract method")
+
+class Dog(Animal):
+    def make_sound(self):
+        return "woof!"
+class Cat(Animal):
+    def make_sound(self):
+        return "mewo!"
+    def make_alt_sound(self):
+        return "meow2"
+def animal_sound(animal):
+    print(animal.make_sound())
+
+dog=Dog()
+
+animal_sound(dog)
+
+
+def myNewFunc():
+    
+    def myNewFuncNested():
+        x="local var"
+    myNewFuncNested()
+    print(x)
+myNewFunc()
+
+from mymodule import myfunc 
+
+myfunc("raj")
